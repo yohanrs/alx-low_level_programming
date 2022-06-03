39818e1159cb40d20e0876f14112660707cb095a
yohanes <johnyemande@gamil.com>
2022-06-03 14:22:06 -0700

    new

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include<stdio.h>
-/*it use puts to print*/
+/*it use puts to print
+ * usen it*/
 int main(void)
 {
-	puts ("\"Programming is like building a multilingual puzzle");
-	return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
