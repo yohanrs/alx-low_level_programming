f579f76f31bc209e2c938eb5fd9080c399d79f44
yohanes <johnyemande@gamil.com>
2022-06-03 14:48:19 -0700

    new

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 /**
- * prinf - use prinf
+ * main - use prinf
  *
  * Description: printf
  * Return: 0
  */
 int main(void)
 {
-	printf("with proper grammer,but the outcome is a piece of art,");
+	printf("ith proper grammar, but the outcome is a piece of art,");
 	return (0);
 }
