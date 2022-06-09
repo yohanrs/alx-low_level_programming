4c34d939c7bbffb4529a236a7d5faabbf6576834
yohanes <johnyemande@gamil.com>
2022-06-09 12:56:35 -0700

    new

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -19,5 +19,6 @@ void print_alphabet_x10(void);
 int _islower(int);
 void positive_or_negative(int);
 int largest_number(int a, int b, int c);
+void print_remaining_days(int month, int day, int year);
 
 #endif /* MAIN_H */
