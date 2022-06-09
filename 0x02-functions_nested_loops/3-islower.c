3990e06d9b4e86c28a4cd8764db2847126a7b2c2
yohanes <johnyemande@gamil.com>
2022-06-09 08:40:54 -0700

    lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * _islower - function
+ *
+ * Return: int
+ */
+int _islower(int c)
+{
+	char i;
+
+	i = char(i);
+
+	if (islower(i))
+		return (1);
+	if (!islower(i))
+		return (0);
+}
