0dc3555aa78f38f070a94cb72f66fea9cedb605c
yohanes <johnyemande@gamil.com>
2022-06-09 00:58:38 -0700

    commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - main
+ *
+ * Description - nothing
+ * Return - 0
+ */
+int main()
+{
+	int i;
+	i = 0;
+	while (i <= 10)
+	{
+		printf("%d", i++);
+	}
+	return (0);
+}
