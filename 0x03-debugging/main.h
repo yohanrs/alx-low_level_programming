bc3f2012b9229a37416e6123338c33c6d2715bbe
yohanes <johnyemande@gamil.com>
2022-06-09 10:20:50 -0700

    new

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * main - main
+ *
+ * Return: 0
+ */
+void print(void)
+{
+	printf("_putchar\n");
+	return;
+}
+void _putchar(char);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int);
+void positive_or_negative(int i);
