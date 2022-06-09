1c545a38fb076c35cdc6979ff3e7436ce02eb485
yohanes <johnyemande@gamil.com>
2022-06-09 08:12:10 -0700

    new

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 /**
- * main - check the code
+ * print_alphabet - check the code
  *
  * Return: Always 0.
  *
@@ -16,5 +16,4 @@ void print_alphabet(void)
 		i += 1;
 	}
 	putchar('\n');
-	return;
 }
