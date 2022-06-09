009cf97772408e43c5c1def2e11437136da48942
yohanes <johnyemande@gamil.com>
2022-06-09 11:07:49 -0700

    new

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -16,6 +16,6 @@ void _putchar(char);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int);
-void positive_or_negative(int i);
+void positive_or_negative(int);
 
 #endif /* MAIN_H */
