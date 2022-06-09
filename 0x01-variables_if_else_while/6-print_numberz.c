30df07a40ee997842f0a66a8fe618261ebd8d277
yohanes <johnyemande@gamil.com>
2022-06-09 01:06:34 -0700

    commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+ * main - main
+ *
+ * Description - nothing
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	while (i < 10)
+	{
+		putchar(i);
+		i += 1;
+	}
+	putchar("\n");
+	return (0);
+}
