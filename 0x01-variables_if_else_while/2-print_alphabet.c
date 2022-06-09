43fa7021f6f69556050cb3caa09dbf312d660667
yohanes <johnyemande@gamil.com>
2022-06-09 00:28:16 -0700

    commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ * main - main
+ *
+ * Description: quote
+ * Return: 0
+ */
+int main(void)
+{
+	char i = 'a';
+
+	while (i <= 'z')
+	{
+		putchar (i);
+		putchar ('\n');
+		i+=1;
+	}
+	return (0);
+}
